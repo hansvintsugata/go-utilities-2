1d79e75c233b7047e1f13ab37c93c6a418227d21
hansvintsugata <hansvintsugata@gmail.com>
2024-01-24 10:32:10 +0700

    utils for config and mandatory

---
+++ config/config.go
@@ -0,0 +1,78 @@
+package zenConfig
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/spf13/viper"
+)
+
+func New(path string, configType string, object interface{}) error {
+	// - check file does exists
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return fmt.Errorf("config file %s does not exists\nerror: %+v", path, err)
+	}
+
+	dir := getDirectory(path)
+	file, err := getFile(path)
+
+	if err != nil {
+		return err
+	}
+
+	v := viper.New()
+	v.SetConfigName(file)
+	v.AddConfigPath(dir)
+	v.SetConfigType(configType)
+	v.AutomaticEnv()
+
+	if err := v.ReadInConfig(); err != nil {
+		return fmt.Errorf("failed to read %s file\nerror: %+v", path, err)
+	}
+
+	if err := v.Unmarshal(&object); err != nil {
+		return fmt.Errorf("failed to unmarshal config to object\nerror: %+v", err)
+	}
+
+	return nil
+}
+
+func NewFromEnv(object interface{}) error {
+	filename := os.Getenv("CONFIG_FILE")
+
+	if filename == "" {
+		filename = ".env"
+	}
+
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		if err := envconfig.Process("", object); err != nil {
+			return fmt.Errorf("%s failed to read from env variable", err)
+		}
+		return nil
+	}
+
+	if err := godotenv.Load(filename); err != nil {
+		return fmt.Errorf("%s failed to read from .env file", err)
+	}
+
+	if err := envconfig.Process("", object); err != nil {
+		return fmt.Errorf("%s failed to read from env variable", err)
+	}
+
+	return nil
+}
+
+func getDirectory(path string) string {
+	splits := strings.Split(path, "/")
+	return strings.Join(splits[:len(splits)-1], "/")
+}
+
+func getFile(path string) (string, error) {
+	splits := strings.Split(path, "/")
+	last := splits[len(splits)-1]
+
+	return last, nil
+}
---
+++ mandatory/user.go
@@ -0,0 +1,33 @@
+package zenMandatory
+
+type User struct {
+	login bool
+	id    uint64
+	email string
+	phone string
+}
+
+func (u User) ID() uint64 {
+	return u.id
+}
+
+func (u User) Email() string {
+	return u.email
+}
+
+func (u User) Phone() string {
+	return u.phone
+}
+
+func (u User) IsLogin() bool {
+	return u.login
+}
+
+func (u User) JSON() map[string]interface{} {
+	return map[string]interface{}{
+		"is_login": u.IsLogin(),
+		"id":       u.ID(),
+		"email":    u.Email(),
+		"phone":    u.Phone(),
+	}
+}
